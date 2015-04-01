17dc0a3ff86d9c2f6c56e4300597f7f4d96cff3c
Matt Mulholland <mulhodm@gmail.com>
2015-04-01 10:52:06 -0400

    first commit

---
+++ week1/pymongo_test.py
@@ -0,0 +1,18 @@
+#!/usr/env python2.7
+
+import pymongo
+from pymongo import MongoClient
+
+# Connect to database
+connection = MongoClient('localhost', 27017)
+
+db = connection.test
+
+# Handle to names collection
+names = db.names
+
+item = names.find_one()
+
+print item['name']
+
+print list(names.find())
